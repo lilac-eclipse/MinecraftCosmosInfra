e0f54f5eb7932052bcd1075f9efbd3378a056273
Lilac Eclipse <lilaceclipse87@gmail.com>
2024-03-31 01:11:49 -0700

    fix: update site bucket naming

--- stack/src/main/kotlin/com/lilaceclipse/minecraftcosmos/stack/config/DeploymentStageInfo.kt
+++ stack/src/main/kotlin/com/lilaceclipse/minecraftcosmos/stack/config/DeploymentStageInfo.kt
@@ -8,13 +8,13 @@ interface DeploymentStageInfo {
 
 class ProdStage(
     override val stageSuffix: String = "prod",
-    override val siteBucketName: String = "cosmos.cryo3.net",
+    override val siteBucketName: String = "cosmos.lilaceclipse.com",
     override val serverDataBucketName: String = "mccosmos-data-prod"
     ) : DeploymentStageInfo
 
 class BetaStage(
     override val stageSuffix: String = "beta",
-    override val siteBucketName: String = "beta.cosmos.cryo3.net",
+    override val siteBucketName: String = "beta.cosmos.lilaceclipse.com",
     override val serverDataBucketName: String = "mccosmos-data-beta"
 ) : DeploymentStageInfo
 
