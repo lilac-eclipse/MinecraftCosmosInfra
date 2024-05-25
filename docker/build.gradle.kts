b150fdd22c5120676ecde17ce2de1ca1e7d00437
Lilac Eclipse <lilaceclipse87@gmail.com>
2024-05-25 15:46:43 -0700

    fix docker runtime issues due to migration

--- docker/build.gradle.kts
+++ docker/build.gradle.kts
@@ -10,6 +10,8 @@
 plugins {
     // Apply the common convention plugin for shared build configuration between library and application projects.
     id("MinecraftCosmosInfra.kotlin-common-conventions")
+
+    application
     kotlin("jvm")
     kotlin("kapt")
 
@@ -17,26 +19,6 @@ plugins {
 }
 
 dependencies {
-//    implementation("com.amazonaws:aws-lambda-java-core:1.2.3")
-//    implementation("com.amazonaws:aws-lambda-java-events:3.11.4")
-//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-//
-//    // AWS client dependencies
-//    implementation("com.amazonaws:aws-java-sdk-sns:1.12.691")
-//    implementation("com.amazonaws:aws-java-sdk-ec2:1.12.691")
-//    implementation("com.amazonaws:aws-java-sdk-ecs:1.12.691")
-//    implementation("software.amazon.awssdk:dynamodb-enhanced:2.25.31")
-//
-//    // Dagger for dependency injection
-//    implementation("com.google.dagger:dagger:2.51.1")
-//    kapt("com.google.dagger:dagger-compiler:2.51.1")
-//
-//    // Logging libraries
-//    implementation("io.github.oshai:kotlin-logging-jvm:5.1.4") // kotlin logging
-//    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
-//    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
-//    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
-
     // Logging libraries
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.4") // kotlin logging
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
@@ -69,4 +51,8 @@ kapt {
     arguments {
         arg("project", "${project.group}${project.name}")
     }
+}
+
+application {
+    mainClass.set("com.lilaceclipse.cosmos.MainKt")
 }
